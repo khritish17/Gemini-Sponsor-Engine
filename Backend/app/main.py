69ef475081fe2e97b09f59a34a3ced1b87f60cb1
Khritish Kumar Behera <khritish.official@gmail.com>
2026-01-30 22:34:02 +0530

    Embeding of cleaned_query

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -6,8 +6,17 @@ GEMINI_API_KEY = settings.gemini_api_key
 models = ["gemini-3-flash-preview", "gemini-3-pro-preview", "gemini-2.5-flash",
           "gemini-2.5-flash-lite", "gemini-2.5-pro", "gemini-2.0-flash",
           "gemini-2.0-flash-lite"]
+embed_models = ["text-embedding-004"]
 
 # Testing codes: Original code will be using fastAPI to handle request from app
-gemini_services = Gemini_Services(gemini_api_key=GEMINI_API_KEY, model_name=models[0], response_schema=Gemini_Response)
+
+# Starting the gemini service
+gemini_services = Gemini_Services(gemini_api_key=GEMINI_API_KEY, embed_model=embed_models[0], model_name=models[0], response_schema=Gemini_Response)
+
+# Response from the Gemini Generative model
 response = gemini_services.ask_gemini(user_querry="I want to learn about why Rust is not considered as a Object oriented Langauge")
-print(response)
+print(response.response)
+
+# Response from the Gemini Embeding model 
+math_vector = gemini_services.get_embeding(response.cleaned_query)
+print(math_vector)
--- Backend/app/services/gemini_services.py
+++ Backend/app/services/gemini_services.py
@@ -1,16 +1,18 @@
 from google import genai
+from google.genai import types
 
 
 class Gemini_Services:
-    def __init__(self, gemini_api_key, response_schema, model_name = "gemini-3-flash-preview"):
+    def __init__(self, gemini_api_key, response_schema, embed_model, model_name = "gemini-3-flash-preview"):
         self.GEMINI_API_KEY = gemini_api_key
         self.model_name = model_name
+        self.embed_model = embed_model
         
         self.client = genai.Client(api_key=self.GEMINI_API_KEY.get_secret_value())
         self.response_schema = response_schema
 
     
-    def ask_gemini(self, user_querry):
+    def ask_gemini(self, user_querry: str):
         response = self.client.models.generate_content(model = self.model_name,
                                                        contents=user_querry,
                                                        config={
@@ -19,3 +21,13 @@ class Gemini_Services:
                                                        }
                                                        )
         return response.parsed
+    
+    def get_embeding(self, cleaned_query: str):
+        response = self.client.models.embed_content(
+            model=self.embed_model,
+            contents=cleaned_query,
+            config=types.EmbedContentConfig(
+                task_type="RETRIEVAL_QUERY"
+            )
+        )
+        return response.embeddings[0].values
